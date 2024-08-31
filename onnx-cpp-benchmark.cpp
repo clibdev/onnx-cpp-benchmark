e751b95260b3c89f442caac834b206de108d03c8
clibdev <clibdev@gmail.com>
2024-08-31 10:50:37 +0300

    style fixes

--- onnx-cpp-benchmark.cpp
+++ onnx-cpp-benchmark.cpp
@@ -11,39 +11,39 @@ std::unique_ptr<Ort::Session> g_ortSession;
 
 std::string convertEnumToString(const ONNXTensorElementDataType value) {
     switch (value) {
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
             return "UNDEFINED";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
             return "FLOAT";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
             return "UINT8";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
             return "INT8";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
             return "UINT16";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
             return "INT16";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
             return "INT32";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
             return "INT64";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
             return "STRING";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
             return "BOOL";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
             return "FLOAT16";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
             return "DOUBLE";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
             return "UINT32";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
             return "UINT64";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
             return "COMPLEX64";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
             return "COMPLEX128";
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
             return "BFLOAT16";
         default:
             return "UNKNOWN";
@@ -51,41 +51,40 @@ std::string convertEnumToString(const ONNXTensorElementDataType value) {
 }
 
 Ort::Value CreateTensor(const ONNXTensorElementDataType orttype, const std::vector<int64_t> &realShape) {
-    Ort::AllocatorWithDefaultOptions allocator;
+    const Ort::AllocatorWithDefaultOptions allocator;
     switch (orttype) {
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
             return Ort::Value::CreateTensor<float>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
             return Ort::Value::CreateTensor<uint8_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
             return Ort::Value::CreateTensor<int8_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
             return Ort::Value::CreateTensor<uint16_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
             return Ort::Value::CreateTensor<int16_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
             return Ort::Value::CreateTensor<int32_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
             return Ort::Value::CreateTensor<int64_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
             return Ort::Value::CreateTensor<bool>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
             return Ort::Value::CreateTensor<double>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
             return Ort::Value::CreateTensor<uint32_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
             return Ort::Value::CreateTensor<uint64_t>(allocator, realShape.data(), realShape.size());
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
         default:
             std::cerr << "onnx-cpp-benchmark: Error in CreateTensor, unable to create tensor of type "
                     << convertEnumToString(orttype) << std::endl;
             std::exit(1);
-            return Ort::Value(nullptr);
     }
 }
 
@@ -96,8 +95,8 @@ void FillTensorWithRandomDataHelper(Ort::Value &tensor, std::vector<int64_t> &re
 
     std::uniform_real_distribution<float> distribution(-100.0, 100.0);
 
-    int tensor_size = 1;
-    for (auto i: realShape) {
+    int64_t tensor_size = 1;
+    for (const auto i: realShape) {
         tensor_size = tensor_size * i;
     }
 
@@ -107,53 +106,46 @@ void FillTensorWithRandomDataHelper(Ort::Value &tensor, std::vector<int64_t> &re
         float random_value = distribution(generator);
         tensor_data[i] = random_value;
     }
-
-    return;
 }
 
 void FillTensorWithRandomData(Ort::Value &tensor) {
-    ONNXTensorElementDataType orttype = tensor.GetTypeInfo().GetTensorTypeAndShapeInfo().GetElementType();
+    const ONNXTensorElementDataType orttype = tensor.GetTypeInfo().GetTensorTypeAndShapeInfo().GetElementType();
     std::vector<int64_t> realShape = tensor.GetTypeInfo().GetTensorTypeAndShapeInfo().GetShape();
     switch (orttype) {
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
             return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
-            return FillTensorWithRandomDataHelper<float>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
             return FillTensorWithRandomDataHelper<uint64_t>(tensor, realShape);
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
+        case ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
         default:
             std::cerr << "onnx-cpp-benchmark: Error in FillTensorWithRandomData, unable to create tensor of type "
                     << convertEnumToString(orttype) << std::endl;
             std::exit(1);
-            return;
     }
 }
 
 int main(int argc, char *argv[]) {
-    // Read the parameters from the command line arguments
-    std::string onnxfilepath = std::string(argv[1]);
+    auto onnxfilepath = std::string(argv[1]);
+    auto backend = std::string(argv[2]);
+
+    if (backend != "cpu" && backend != "cuda") {
+        std::cerr << "ERROR: Unsupported backend " << backend << std::endl;
+        return 1;
+    }
 
     // Number of inputs fed to the network fed to the network in one inference run
     int64_t nrOfParallelInputsInOneIteration = 10;
@@ -161,45 +153,21 @@ int main(int argc, char *argv[]) {
     // Number of iterations
     int64_t numberOfIterations = 1000;
 
-    // Backend
-    std::string backend = std::string(argv[2]);
-
-    if (backend != "cpu" && backend != "cuda") {
-        std::cerr << "ERROR: Unsupported backend " << backend << std::endl;
-        return 1;
-    }
-
     OrtSessionOptions *sessionOptionsCPtr;
-    OrtStatus *statusCreateSessionPtr = Ort::GetApi().CreateSessionOptions(&sessionOptionsCPtr);
-
-    // TODO handle statusCreateSessionPtr
 
     // Enable CUDA if requested
     OrtCUDAProviderOptionsV2 *cuda_options = nullptr;
     if (backend == "cuda") {
-        // Check that onnxruntime is compiled with GPU support
-        const auto providers = Ort::GetAvailableProviders();
-
-        if (std::find(providers.begin(), providers.end(), "CUDAExecutionProvider") == providers.end()) {
-            std::cerr << "onnxruntime is not compiled with the cuda provide, impossible to use onnxruntimecuda backend"
-                    << std::endl;
-            std::cerr << "Available onnxruntime provides: " << std::endl;
-            for (const auto &provider: providers) {
-                std::cerr << provider << '\n';
-            }
+        OrtStatus *status = Ort::GetApi().CreateCUDAProviderOptions(&cuda_options);
+        if (Ort::Status statusCxx(status); !statusCxx.IsOK()) {
+            std::cerr << "CreateCUDAProviderOptions error " << statusCxx.GetErrorMessage() << std::endl;
             return 1;
         }
 
-        Ort::GetApi().CreateCUDAProviderOptions(&cuda_options);
-
-        OrtStatus *statusPtr = Ort::GetApi().SessionOptionsAppendExecutionProvider_CUDA_V2(
-            sessionOptionsCPtr, cuda_options);
-
-        Ort::Status statusCxx(statusPtr);
-
-        if (!statusCxx.IsOK()) {
-            std::cerr << "SessionOptionsAppendExecutionProvider_CUDA_V2 returned error " << statusCxx.GetErrorMessage()
-                    << std::endl;
+        status = Ort::GetApi().SessionOptionsAppendExecutionProvider_CUDA_V2(sessionOptionsCPtr, cuda_options);
+        if (Ort::Status statusCxx(status); !statusCxx.IsOK()) {
+            std::cerr << "SessionOptionsAppendExecutionProvider_CUDA_V2 error " << statusCxx.GetErrorMessage() <<
+                    std::endl;
             return 1;
         }
     }
@@ -235,37 +203,29 @@ int main(int argc, char *argv[]) {
         outputsInfo.push_back(g_ortSession->GetOutputTypeInfo(outputIdx));
     }
 
-    bool verbose = true;
+    std::cout << "Model " << onnxfilepath << " has " << inputsInfo.size() << " inputs and " << outputsInfo.size()
+            << " outputs." << std::endl;
 
-    if (verbose) {
-        std::cout << "Model " << onnxfilepath << " has " << inputsInfo.size() << " inputs and " << outputsInfo.size() <<
-                " outputs." << std::endl;
-
-        for (size_t inputIdx = 0; inputIdx < g_ortSession->GetInputCount(); inputIdx++) {
-            std::cout << "  Input " << inputIdx << " name: " << inputNames[inputIdx]
-                    << " type: " << convertEnumToString(
-                        inputsInfo[inputIdx].GetTensorTypeAndShapeInfo().GetElementType())
-                    //<< " elementCount: " << (inputsInfo[inputIdx].GetTensorTypeAndShapeInfo().GetElementCount())
-                    << " shape: ";
-            auto shape = inputsInfo[inputIdx].GetTensorTypeAndShapeInfo().GetShape();
-            for (auto i: shape) {
-                std::cout << i << ",";
-            }
-            std::cout << std::endl;
+    for (size_t inputIdx = 0; inputIdx < g_ortSession->GetInputCount(); inputIdx++) {
+        std::cout << "  Input " << inputIdx << " name: " << inputNames[inputIdx]
+                << " type: " << convertEnumToString(inputsInfo[inputIdx].GetTensorTypeAndShapeInfo().GetElementType())
+                << " shape: ";
+        auto shape = inputsInfo[inputIdx].GetTensorTypeAndShapeInfo().GetShape();
+        for (auto i: shape) {
+            std::cout << i << ",";
         }
+        std::cout << std::endl;
+    }
 
-        for (size_t outputIdx = 0; outputIdx < g_ortSession->GetOutputCount(); outputIdx++) {
-            std::cout << "  Output " << outputIdx << " name: " << outputNames[outputIdx]
-                    << " type: " << convertEnumToString(
-                        outputsInfo[outputIdx].GetTensorTypeAndShapeInfo().GetElementType())
-                    //<< " elementCount: " << outputsInfo[outputIdx].GetTensorTypeAndShapeInfo().GetElementCount()
-                    << " shape: ";
-            auto shape = outputsInfo[outputIdx].GetTensorTypeAndShapeInfo().GetShape();
-            for (auto i: shape) {
-                std::cout << i << ",";
-            }
-            std::cout << std::endl;
+    for (size_t outputIdx = 0; outputIdx < g_ortSession->GetOutputCount(); outputIdx++) {
+        std::cout << "  Output " << outputIdx << " name: " << outputNames[outputIdx]
+                << " type: " << convertEnumToString(outputsInfo[outputIdx].GetTensorTypeAndShapeInfo().GetElementType())
+                << " shape: ";
+        auto shape = outputsInfo[outputIdx].GetTensorTypeAndShapeInfo().GetShape();
+        for (auto i: shape) {
+            std::cout << i << ",";
         }
+        std::cout << std::endl;
     }
 
     std::vector<Ort::Value> inputTensors;
@@ -307,8 +267,8 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << "iterations (i.e. number of inference run used for benchmark): " << numberOfIterations << std::endl;
-    std::cout << "batch_size (i.e. number of parallel inputs fed to network in one run/iteration): " <<
-            nrOfParallelInputsInOneIteration << std::endl;
+    std::cout << "batch_size (i.e. number of parallel inputs fed to network in one run/iteration): "
+            << nrOfParallelInputsInOneIteration << std::endl;
 
     std::vector<double> durations;
     for (size_t iter = 0; iter < numberOfIterations; iter++) {
@@ -326,25 +286,26 @@ int main(int argc, char *argv[]) {
                           outputNames_cstr.size());
         auto end_time = std::chrono::high_resolution_clock::now();
         auto duration = end_time - start_time;
-        double duration_s = duration.count() / 1000000.0;
+        double duration_s = static_cast<double>(duration.count()) / 1000000.0;
         durations.push_back(duration_s);
     }
 
     double sum = std::accumulate(std::begin(durations), std::end(durations), 0.0);
-    double m = sum / durations.size();
+    double m = sum / static_cast<double>(durations.size());
     double accum = 0.0;
     std::for_each(std::begin(durations), std::end(durations), [&](const double d) {
         accum += (d - m) * (d - m);
     });
 
-    double stdev = sqrt(accum / (durations.size() - 1));
+    double stdev = sqrt(accum / (static_cast<double>(durations.size()) - 1));
 
     std::cerr << "=======================" << std::endl;
     std::cerr << "======= Results =======" << std::endl;
     std::cerr << "=======================" << std::endl;
 
     std::cerr << "Time (in seconds) for inference: mean:  " << m << " stddev:" << stdev << std::endl;
-    std::cerr << "Time (in seconds) for input: mean:  " << m / nrOfParallelInputsInOneIteration << std::endl;
+    std::cerr << "Time (in seconds) for input: mean:  " << m / static_cast<double>(nrOfParallelInputsInOneIteration)
+            << std::endl;
 
     if (backend == "cuda") {
         Ort::GetApi().ReleaseCUDAProviderOptions(cuda_options);
